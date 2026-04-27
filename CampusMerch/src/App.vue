22feeace250a4eb1809b2654e3d5402f877999a3
CandyRain <972536599@qq.com>
2026-04-27 20:03:06 +0800

    搭路由

--- CampusMerch/src/App.vue
+++ CampusMerch/src/App.vue
@@ -1,7 +1,6 @@
 <script setup></script>
 
-<template>、
-1
+<template>
 <router-view/>
 </template>
 
--- CampusMerch/src/router/index.js
+++ CampusMerch/src/router/index.js
@@ -1,27 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import login from '../login.vue'
 import register from '../views/Customer/register.vue'
-import LeaderPage from '../LeaderPage.vue'
+import CustomerHomepage from '../views/Customer/CustomerHomepage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'LeaderPage',
-      component: LeaderPage,
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: login,
-    },
+      name: 'CustomerHomepage',
+      component: CustomerHomepage,
+    }, 
     {
       path: '/register',
       name: 'register',
       component: register,
     },
-    
   ],
 })
 
