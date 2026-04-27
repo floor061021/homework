76efaf8bfd12a5eadd5b366f73077435b8c983ac
CandyRain <972536599@qq.com>
2026-04-27 10:40:40 +0800

    添加组件、配置环境

--- CampusMerch/src/main.js
+++ CampusMerch/src/main.js
@@ -4,9 +4,19 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// 1. 引入 Element Plus 组件库和样式
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 const app = createApp(App)
 
+// 2. 注册 Pinia
 app.use(createPinia())
+
+// 3. 注册 路由
 app.use(router)
+
+// 4. 注册 Element Plus
+app.use(ElementPlus)
 
 app.mount('#app')
